85e7b1d30cbef220c90ccfb0e4bb57e3006ee9ca
Justin <justinpostigo@yahoo.com>
2016-11-08 15:46:40 -0800

    adding python files

---
+++ GenerateCSV.py
@@ -0,0 +1,28 @@
+import re, os, random
+
+PATH = "../Drexel-AMT-Corpus/"
+
+#CSV format
+#AA,path_to_file
+#,path_to_file - to classify
+
+fp = open("leave_one_out.csv", "w")
+
+for folder in os.listdir(PATH):
+    #print(folder)
+    #count = 0
+    file_arr = []
+    for file in os.listdir(PATH + "/" + folder):
+        if(not file.split('_')[1].isnumeric()):
+            index = random.randint(0, len(file_arr) - 1)
+            file_to_classify = file_arr[index]
+            new_file = ',' + file_to_classify.split(',')[1]
+            file_arr.remove(file_to_classify)
+            fp.write(new_file + '\n')
+            for f in file_arr:
+                fp.write(f + '\n')
+            break
+        file_name = folder + ',' + PATH + file
+        print(file_name)
+        file_arr.append(file_name)
+
