f4922ea13db5b221702d602c652fe5e657a508e7
Justin <justinpostigo@yahoo.com>
2016-11-10 16:09:42 -0800

    added synonym replacement

--- GenerateCSV.py
+++ GenerateCSV.py
@@ -1,4 +1,60 @@
-import os, random
+import os, random, re
+import nltk
+from nltk.corpus import wordnet
+from nltk.corpus import stopwords
+from nltk.tokenize import WhitespaceTokenizer
+
+punctuation = ",;.!?"
+
+def replacement(txt_file):
+    print(txt_file)
+    stops = set(stopwords.words('english')+['I'])
+    fp = open(txt_file,encoding='utf-8')
+    content = fp.read()
+    fp.close()
+
+
+    token = nltk.word_tokenize(content)
+    pos = nltk.pos_tag(token)
+
+    output = ""
+
+    for word, speech in pos:
+        if speech == 'JJ':
+            syn = wordnet.synsets(word)
+            if syn:
+                print(word + ', ' + str(syn))
+                output += ' ' + str(syn[0].lemma_names('eng')[0])
+            else:
+                output += ' ' + word
+        else:
+            if(word in punctuation):
+                output += word
+            else:
+                output += ' ' + word
+
+    output = re.sub(r" ''",'"', output)
+    output = re.sub(r'`` ', '"' , output)
+    output = re.sub(r'\( ', '(', output)
+    output = re.sub(r' \)', ')', output)
+    output = re.sub(r" '", "'", output)
+
+
+    '''
+
+    for word in token:
+        syn = wordnet.synsets(word)
+        if word not in stops and syn:
+            print(syn[0].lemma_names('eng')[0])
+            output += ' ' + str(syn[0].lemma_names('eng')[0])
+        else:
+            output += ' ' + word
+    '''
+    newFp = open('obfucated.txt','w')
+    newFp.write(output)
+    newFp.close()
+    exit(2)
+
 
 PATH = os.getcwd() + os.sep + "Drexel-AMT-Corpus" + os.sep
 
@@ -15,6 +71,9 @@ for folder in os.listdir(PATH):
         if(not file.split('_')[1].isnumeric()):
             index = random.randint(0, len(file_arr) - 1)
             file_to_classify = file_arr[index]
+            #replacement(file_to_classify)
+            replacement(os.getcwd() + os.sep +
+                        "Drexel-AMT-Corpus" + os.sep + 'aa' + os.sep + file_to_classify.split(os.sep)[9])
             new_file = ',' + file_to_classify.split(',')[1]
             file_arr.remove(file_to_classify)
             fp.write(new_file + '\n')
@@ -23,4 +82,3 @@ for folder in os.listdir(PATH):
             break
         file_name = folder + ',' + PATH + folder + os.sep + file
         file_arr.append(file_name)
-
